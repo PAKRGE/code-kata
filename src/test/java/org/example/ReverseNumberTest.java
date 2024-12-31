fa292e45ee6b02c91dd592ec68b4fe7c24cb9d13
PARKGE <phg5459@naver.com>
2024-12-31 17:24:39 +0900

    ReverseNumberTest 코드 수정

--- src/test/java/org/example/ReverseNumberTest.java
+++ src/test/java/org/example/ReverseNumberTest.java
@@ -3,30 +3,40 @@ package org.example;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 @DisplayName("숫자 뒤집기")
 class ReverseNumberTest {
     @Test
     void testPositiveNumber() {
         int inputNum = 123;
-        ReverseNumber.solution(inputNum);
+        int result = ReverseNumber.solution(inputNum);
+        int actual = 321;
+        assertEquals(result, actual);
     }
 
     @Test
     void testNegativeNumber() {
         int inputNum = -123;
-        ReverseNumber.solution(inputNum);
+        int result = ReverseNumber.solution(inputNum);
+        int actual = -321;
+        assertEquals(result, actual);
     }
 
     @Test
     void testSingleDigit() {
         int inputNum = 5;
-        ReverseNumber.solution(inputNum);
+        int result = ReverseNumber.solution(inputNum);
+        int actual = 5;
+        assertEquals(result, actual);
     }
 
     @Test
     void testZero() {
         int inputNum = 0;
-        ReverseNumber.solution(inputNum);
+        int result = ReverseNumber.solution(inputNum);
+        int actual = 0;
+        assertEquals(result, actual);
     }
 }
